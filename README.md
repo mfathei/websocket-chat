5fb5d8ecf7713405b1361d25b924450382432487
Mohamed Mahdy <mohamed.mahdy@sphinxinfotech.net>
2017-12-28 10:21:17 +0200

    adjust style and send the same message for all clients except the sender

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Run 
 
-navigate to server folder and run `npm install` to install pachages then `node index.js` or `nodemon index.js`
+navigate to server folder and run `npm install` to install packages then `node index.js` or `nodemon index.js`
 
 now open client.html in your browser many times and start chatting 
 
--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 
 var server = require('ws').Server;
-var s = new server({port : 5001});
+var s = new server({ port: 5001 });
 
-s.on("connection", function(ws){
-    ws.on("message", function(message){
+s.on("connection", function (ws) {
+    ws.on("message", function (message) {
         console.log("Received: " + message);
-        ws.send("From server: " + message);
+
+        s.clients.forEach(function (client) {
+            if(client === ws) return;
+            client.send(message);
+        });
+
+        // ws.send("From server: " + message);
+    });
+
+    ws.on("close", function () {
+        console.log("I lost a client");
     });
 });
